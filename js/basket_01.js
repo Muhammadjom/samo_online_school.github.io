6a4e8b2ddf56087af810c3f17411cff2ffb598b4
Muhammadjom <160703785+Muhammadjom@users.noreply.github.com>
2024-11-21 10:51:50 +0500

    Add files via upload

---
+++ js/basket_01.js
@@ -0,0 +1,81 @@
+"use strict"
+//==========================================
+import { ERROR_SERVER, NO_ITEMS_CART } from './constants.js';
+import { 
+    showErrorMessage,
+    setBasketLocalStorage,
+    getBasketLocalStorage,
+    checkingRelevanceValueBasket
+} from './utils.js';
+
+const cart = document.querySelector('.cart');
+let productsData = [];
+
+
+getProducts();
+cart.addEventListener('click', delProductBasket);
+
+
+async function getProducts() {
+    try {
+
+        if (!productsData.length) {
+            const res = await fetch('../data/products.json');
+            if (!res.ok) {
+                throw new Error(res.statusText)
+            }
+            productsData = await res.json();
+        }
+        
+        loadProductBasket(productsData);
+
+    } catch (err) {
+        showErrorMessage(ERROR_SERVER);
+        console.log(err.message);
+    }
+}
+
+function loadProductBasket(data) {
+    cart.textContent = '';
+
+    if (!data || !data.length) {
+        showErrorMessage(ERROR_SERVER)
+        return;
+    }
+
+    checkingRelevanceValueBasket(data);
+    const basket = getBasketLocalStorage();
+
+    if(!basket || !basket.length) {
+        showErrorMessage(NO_ITEMS_CART)
+        return;
+    }
+
+    const findProducts = data.filter(item => basket.includes(String(item.id)));
+
+    if(!findProducts.length) {
+        showErrorMessage(NO_ITEMS_CART)
+        return;
+    }
+
+    renderProductsBasket(findProducts);
+}
+
+
+function renderProductsBasket(arr) {
+    arr.forEach(card => {
+        const { id, img, title} = card;
+
+        const cardItem = 
+        `
+        <div class="cart__product" data-product-id="${id}">
+            <div class="cart__img">
+                <img src="./images/${img}" alt="${title}">
+            </div>
+        </div>
+        `;
+        
+        cart.insertAdjacentHTML('beforeend', cardItem);
+    });
+
+}
